a1e1ea86ddefcc03a4f8418df8a0ba2912669d57
Mukundhapriya <sankumukundhapriya@gmail.com>
2026-07-02 10:54:20 +0530

    Fix CORS configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,30 +4,36 @@ import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import morgan from "morgan";
 import atsRoutes from "./routes/ats.routes";
-
-
 import routes from "./routes";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://resume-iq-ai-chi.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
-
 app.use(helmet());
-
 app.use(morgan("dev"));
 
 app.use("/api", routes);
+app.use("/api/ats", atsRoutes);
 
 app.get("/", (_req, res) => {
   res.status(200).json({
@@ -35,7 +41,5 @@ app.get("/", (_req, res) => {
     message: "ResumeIQ AI Backend Running",
   });
 });
-
-app.use("/api/ats", atsRoutes);
 
 export default app;
